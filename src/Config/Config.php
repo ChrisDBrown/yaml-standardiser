e1eb289f4d5c1a1336ee875ddf718153bc8b972b
Chris D Brown <owner@chrisdbrown.co.uk>
2019-05-01 16:32:28 +0100

    feat: Add default config object

--- src/Config/Config.php
+++ src/Config/Config.php
@@ -1,9 +1,35 @@
 <?php
-declare(strict_types=1);
+
+declare(strict_types = 1);
 
 namespace YamlStandardiser\Config;
 
 class Config
 {
+
+	public function getIndentType(): string
+	{
+		return 'spaces';
+	}
+
+	public function getIndentAmount(): int
+	{
+		return 2;
+	}
+
+	public function getKeyAlphabetisationDepth(): int
+	{
+		return 2;
+	}
+
+	public function getPrioritisedTopLevelKeys(): array
+	{
+		return ['parameters', 'services'];
+	}
+
+	public function getSpaceBetweenBlocksAmount(): int
+	{
+		return 0;
+	}
 
 }
